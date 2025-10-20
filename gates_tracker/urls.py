11254c56d0d4e08c3e12007c59cded6bf2d11728
Jeremiah Sylvester <jesylvesterboy@gmail.com>
2025-10-20 10:29:47 +0100

    restore: Original homepage structure - sales available at /sales/

--- gates_tracker/urls.py
+++ gates_tracker/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sales/', include('sales.urls')),
-    path('', RedirectView.as_view(url='/sales/', permanent=False)),
+    # ... keep your existing URLs here ...
 ]
 
 if settings.DEBUG:
