190c4f3373f09bf4e87d7f39c64a3531d4e10b42
Jeremiah Sylvester <jesylvesterboy@gmail.com>
2025-10-30 09:44:58 +0100

    PROFESSIONAL FIX: Update urls.py import to include root_view

--- gates_tracker/urls.py
+++ gates_tracker/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import fss_tracker_dashboard, system_health_check
+from .views import fss_tracker_dashboard, system_health_check, root_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
