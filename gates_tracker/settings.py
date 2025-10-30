03f36febd74affc4e378fa97832603a9972932c7
Jeremiah Sylvester <jesylvesterboy@gmail.com>
2025-10-30 10:20:42 +0100

    PROFESSIONAL FIX: Template configuration and dual template locations

--- gates_tracker/settings.py
+++ gates_tracker/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'gates_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'gates_tracker' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
